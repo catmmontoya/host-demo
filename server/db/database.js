a1abaea53d94635d3028a682d535c368b7172f30
Caitlin Baldini <cat@listoglobal.com>
2024-04-03 11:22:36 -0600

    add password

--- server/db/database.js
+++ server/db/database.js
@@ -1,24 +1,25 @@
 import { Sequelize } from "sequelize";
 
 const connectToDB = async (dbURI) => {
-    console.log(`Connecting to DB: ${dbURI}`)
+  console.log(`Connecting to DB: ${dbURI}`);
 
-    const sequelize = new Sequelize(dbURI, {
-        logging: false,
-        define: {
-            underscored: true,
-            timestamps: false,
-        }
-    })
+  const sequelize = new Sequelize(dbURI, {
+    logging: false,
+    define: {
+      underscored: true,
+      timestamps: false,
+    },
+    password: "admin",
+  });
 
-    try {
-        await sequelize.authenticate()
-        console.log('Connected to DB successfully')
-    } catch (error) {
-        console.log(`Unable to connect to DB: ${error}`)
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Connected to DB successfully");
+  } catch (error) {
+    console.log(`Unable to connect to DB: ${error}`);
+  }
 
-    return sequelize
-}
+  return sequelize;
+};
 
-export default connectToDB
+export default connectToDB;
